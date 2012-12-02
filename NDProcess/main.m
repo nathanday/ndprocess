3ee7953b80b4c1f3822fba7ae6c6f7726d89ed62
Nathan Day <nathan_day@mac.com>
2012-12-02 17:23:05 +1100

    Initial Commit

---
+++ NDProcess/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NDProcess
+//
+//  Created by Nathan Day on 2/12/12.
+//  Copyright (c) 2012 Nathan Day. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
