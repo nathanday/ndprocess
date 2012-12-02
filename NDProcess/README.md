cee30ceb11b045ea6a26172d60925c24bbfc1221
Nathan Day <nathan_day@mac.com>
2012-12-02 17:28:19 +1100

    Some updates to get ready for uploading to GitHub

---
+++ NDProcess/README.md
@@ -0,0 +1,11 @@
+# NDProcess
+
+##About
+Added open request, used to be on my .mac account, could be updated a bit to take advantage of modern Cocoa/Objective-C features. Is a wrapper for Apples Carboin process library.
+
+### Licensing
+**NDJSON** using an MIT-style license which means use as you wish just give me credit where appropriate.
+
+### Automatic Reference Counting.
+**NDJSON** can be used in *Automatic Reference Counting* projects by setting the flag *-fno-objc-arc* for the .m files, this turns off *Automatic Reference Counting* for just these files. The NSProcess class itself could be updated to work with automatic reference counting.
+
--- NDProcess/main.m
+++ NDProcess/main.m
@@ -1,22 +1,47 @@
-//
-//  main.m
-//  NDProcess
-//
-//  Created by Nathan Day on 2/12/12.
-//  Copyright (c) 2012 Nathan Day. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
+#import "NDProcess.h"
 
-int main(int argc, const char * argv[])
+int main (int argc, const char * argv[])
 {
+	NSAutoreleasePool			* pool = [[NSAutoreleasePool alloc] init];
+	NSArray						* theProcessArray;
+	NDProcess					* theFrontProcess,
+	* theCurrentProcess,
+	* theNamedProcess;
+	int							theChar;
 
-	@autoreleasepool {
-	    
-	    // insert code here...
-	    NSLog(@"Hello, World!");
-	    
+	theProcessArray = [NDProcess everyProcess];
+
+	printf("every processes:\n%s\n\n", [[theProcessArray description] UTF8String]);
+
+	theFrontProcess = [NDProcess frontProcess];
+	printf("front process:\n\t%s\n\tlocation:%s\n", [[theFrontProcess description] UTF8String], [[theFrontProcess path] UTF8String]);
+	printf("launched by:\n\t%s\n\n", [[[theFrontProcess launcher] description] UTF8String]);
+
+	//	[[theFrontProcess retain] release];				// test reuses of instance
+	theCurrentProcess = [NDProcess currentProcess];
+	printf("current process:\n\t%s\n\tlocation:%s\n", [[theCurrentProcess description] UTF8String], [[theFrontProcess path] UTF8String]);
+	printf("launched by:\n\t%s\n\n", [[[theCurrentProcess launcher] description] UTF8String]);
+
+	theNamedProcess = [NDProcess firstProcessNamed:@"TextEdit"];
+	if( theNamedProcess )
+	{
+		printf( "Waiting while you quit TextEdit, then press return to continue\n");
+		theChar = getchar();
+
+		if( [theNamedProcess isValid] )
+		{
+			printf("TextEdit process:\n\t%s, mode:%x\n\tlocation:%s\n", [[theNamedProcess description] UTF8String], (unsigned int)[theNamedProcess mode], [[theNamedProcess path] UTF8String] );
+			printf("launched by:\n\t%s\n\n", [[[theNamedProcess launcher] description] UTF8String]);
+		}
+		else
+			printf("\nTextEdit process is invalid\n" );
 	}
-    return 0;
+	else
+		printf( "Attempt to get TextEdit process returned nil\n");
+
+	[pool release];
+	return 0;
 }
+
 
